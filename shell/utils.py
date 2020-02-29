117dac9755a533a48d673ffc3313a51ab90a7e68
davindicode <Davindi09@gmail.com>
2020-02-29 19:27:47 +0000

    download

--- shell/utils.py
+++ shell/utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import urllib.request
+import zipfile
+import os
 
 def rm_nan(X, axis=0):
     """
@@ -13,3 +16,22 @@ def rm_nan(X, axis=0):
     ind_cut = np.unique(where_nan[axis])
     X_ = np.delete(X, ind_cut, axis)
     return X_
+
+def download_data()
+    """
+    Download and extract the shell challenge data
+    """
+    print("Starting download ...")
+    urls = [
+        "https://github.com/aihack20/shell_challenge/releases/download/data/clean_dataset.zip",
+        "https://github.com/aihack20/shell_challenge/releases/download/data/raw_dataset.zip",
+    ]
+    os.makedirs("shell_data", exist_ok=True)
+    for url in urls:
+        with urllib.request.urlopen(url) as src:
+            with open("tmp.zip", "wb") as dest:
+                dest.write(src.read())
+        print("Unpacking archive ...")
+        with zipfile.ZipFile("tmp.zip") as f:
+            f.extractall("shell_data")
+    print("Done!")
